b61e7544f704f56147a3785a71a62d1b18b110f1
Burak Cüce <b.cuece@web.de>
2022-07-02 14:42:15 +0200

    Initial Commit

---
+++ Photo Editing App/Photo_Editing_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  Photo_Editing_AppApp.swift
+//  Photo Editing App
+//
+//  Created by Burak Cüce on 02.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Editing_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
